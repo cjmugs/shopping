016097796e78150a5d672b29e3e97f0c3b7f48ee
Chris <cjmugs@gmail.com>
2021-06-29 11:43:22 -0500

    updates

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
 import shopping.shopping_cart
+import utility
 
 print(shopping.shopping_cart)
 print(shopping.shopping_cart.buy("HD Tv"))
 print(shopping.shopping_cart.buy("Fancy Sports Car"))
+print(utility.multiply(100, 420))
+print(utility.divide(60, 420))
